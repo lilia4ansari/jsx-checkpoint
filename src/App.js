9ff4b59562e9ffe07719bcfc0c1f305632016fbd
lilia4ansari <liliaansari44@gmail.com>
2021-12-21 17:33:03 +0100

    finich

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import flower from './imageInSrc/flowers.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+        <div style={{"border":"solid 1px black","max-width":"100vw"}}>
+           <h1 className="title red">Your name here</h1>
+            <br />
+        <img src={flower}/>
+            <br />
+           <img src="./OIP.jpg" />
+        </div>
+        <video width={320} height={240} controls>
+          <source src="myVideo.mp4" type="video/mp4" />
+        </video>
+    </div> 
   );
 }
 
